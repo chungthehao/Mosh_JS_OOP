d9f8b0dcbc4681be8e315062d3ee7c0fe8338192
thehao <chungthehao@gmail.com>
2018-12-13 20:28:59 +0700

    007 Object Literals

---
+++ 02 Objects/007 Object Literals/index.js
@@ -0,0 +1,14 @@
+
+// Using OBJECT LITERAL SYNTAX {}
+const circle = {
+  radius: 1,
+  location: {
+    x: 2,
+    y: 8
+  },
+  draw: function() {
+    console.log('Draw');
+  }
+};
+
+circle.draw();
